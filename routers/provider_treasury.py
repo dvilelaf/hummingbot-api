b4ecf87b0a5229854c1557d449d79eb899cb1152
dvilela <dvilelaf@gmail.com>
2026-07-15 13:08:00 +0200

    fix: surface treasury capacity diagnostics

--- routers/provider_treasury.py
+++ routers/provider_treasury.py
@@ -455,7 +455,7 @@ async def _build_hyperliquid_egress(
         accounts_service,
         str(stored_request["account_name"]),
     )
-    logger.info(
+    logger.warning(
         "Hyperliquid treasury capacity check: available_usdc=%s required_usdc=%s",
         source_baseline,
         source_debit,
